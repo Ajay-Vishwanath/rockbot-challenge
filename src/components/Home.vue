c1947fc6849cd7d2dac82d18e188d78ffe551a1e
Ajay <ajayvishwanath.94@gmail.com>
2020-02-24 19:26:36 -0800

    Write Axios Call and Refresh Data Every 30 Seconds

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,27 +1,39 @@
 <template>
   <div>
-     <p>Home Page</p>
-     {{ data }}
+     <NowPlaying :nowPlaying=data.aNowPlaying />
   </div>
 </template>
 
 <script>
+import NowPlaying from './Now_Playing'
 import axios from 'axios'
 
 export default {
+    components : {
+    'NowPlaying': NowPlaying
+  },
   data(){
     return{
-      data: null
+      data: null,
+      timer: ''
     }
   },
   name: 'Home',
-  mounted () {
-    axios
+  created () {
+    this.fetchData();
+    //timer to make another axios call to the API every 30 seconds
+    this.timer = setInterval(this.fetchData, 30000)
+  },
+  methods: {
+    fetchData () {
+      axios
       .get('https://api.rockbot.com/v2/venue/10')
       .then(response => (this.data = response.data))
+    }
   },
-  beforeUpdate(){
-    debugger
+  beforeDestroy() {
+    clearInterval((this.timer))
   }
 }
+
 </script>
---
+++ src/components/Now_Playing.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+     {{ nowPlaying }}
+  </div>
+</template>
+
+<script>
+
+export default {
+  props: [
+      'nowPlaying',
+      ],
+  name: 'NowPlaying'
+}
+</script>
