4c118245efaadb5e615e8558a5559e7b632ab88c
Ajay <ajayvishwanath.94@gmail.com>
2020-02-24 17:38:49 -0800

    Configure Routes

---
+++ src/components/Navbar.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+     <p>NAVBAR</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar'
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,25 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+
+import Navbar from './components/Navbar'
+import Leaderboard from './components/leaderboard'
+
+Vue.use(VueRouter)
+
+const routes = [
+  { path: '/', component: Navbar },
+  { path: '/leaderboard', component: Leaderboard}
+];
+
+const router = new VueRouter({
+  routes,
+  mode: 'history'
+})
 
 new Vue({
   el: '#app',
-  render: h => h(App)
-})
+  template: '<App/>',
+  components: { App },
+  router
+}).$mount('#app')
