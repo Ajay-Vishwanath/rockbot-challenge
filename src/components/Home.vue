dd1a8484307d1b4f9a3067b10fdf157f646b8534
Ajay <ajayvishwanath.94@gmail.com>
2020-02-24 18:50:06 -0800

    Add Favicon

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,11 +1,27 @@
 <template>
   <div>
      <p>Home Page</p>
+     {{ data }}
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
-  name: 'Home'
+  data(){
+    return{
+      data: null
+    }
+  },
+  name: 'Home',
+  mounted () {
+    axios
+      .get('https://api.rockbot.com/v2/venue/10')
+      .then(response => (this.data = response.data))
+  },
+  beforeUpdate(){
+    debugger
+  }
 }
 </script>
