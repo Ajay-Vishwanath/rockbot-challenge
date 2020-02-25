be6e1e80d16ee921cac913666751fbb336e0354f
Ajay <ajayvishwanath.94@gmail.com>
2020-02-25 13:20:28 -0800

    Fix Now Playing Ordering Mistake

--- src/components/Now_Playing.vue
+++ src/components/Now_Playing.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="now-playing-container">
-    <img v-bind:src="nowPlaying.sArtwork" alt="">
+    <img v-bind:src="nowPlaying.sArtwork">
     <div>
-      <p id="now-playing-title">{{nowPlaying.sSong}}</p>
       <p id="now-playing-artist">{{nowPlaying.sArtist}}</p>
+      <p id="now-playing-title">{{nowPlaying.sSong}}</p>
     </div>
   </div>
 </template>
